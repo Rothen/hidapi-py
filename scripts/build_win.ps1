6b3265b9e19d674c5b88a358d9441b994162f7fc
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2025-04-11 09:57:32 +0200

    Add Windows build workflow and update build script for hidapi-py

--- scripts/build_win.ps1
+++ scripts/build_win.ps1
@@ -2,7 +2,6 @@ cmake `
     -S . `
     -B build `
     -G Ninja `
-    -DCMAKE_BUILD_TYPE=Release `
-    -DPYTHON_EXECUTABLE="${env:python_path}\python.exe"
+    -DCMAKE_BUILD_TYPE=Release
 
 cmake --build build --config Release --clean-first --target hidapi-py
