7d08160f2d978c0d796df1a1585ad4b46799e248
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2025-04-11 16:25:28 +0200

    Refactor HidDeviceInfo class: rename member variable from hid_device_info to device_info for clarity

--- includes/hid_device_info.h
+++ includes/hid_device_info.h
@@ -21,26 +21,26 @@ namespace py = pybind11;
 
 class HidDeviceInfo {
 public:
-    HidDeviceInfo() : hid_device_info({}) {}
-    HidDeviceInfo(hid_device_info *ptr) : hid_device_info(*ptr) {}
-    HidDeviceInfo(HidDevice &hid_device) : hid_device_info(*hid_get_device_info(hid_device.get_device())) {}
+    HidDeviceInfo() : device_info({}) {}
+    HidDeviceInfo(hid_device_info *ptr) : device_info(*ptr) {}
+    HidDeviceInfo(HidDevice &hid_device) : device_info(*hid_get_device_info(hid_device.get_device())) {}
 
-    std::string get_path() {return hid_device_info.path;}
-    unsigned short get_vendor_id() { return hid_device_info.vendor_id; }
-    unsigned short get_product_id() { return hid_device_info.product_id; }
-    std::wstring get_serial_number() { return hid_device_info.serial_number; }
-    unsigned short get_release_number() { return hid_device_info.release_number; }
-    std::wstring get_manufacturer_string() { return std::wstring(hid_device_info.manufacturer_string); }
-    std::wstring get_product_string() { return std::wstring(hid_device_info.product_string); }
-    unsigned short get_usage_page() { return hid_device_info.usage_page; }
-    unsigned short get_usage() { return hid_device_info.usage; }
-    int get_interface_number() { return hid_device_info.interface_number; }
-    hid_bus_type get_bus_type() { return hid_device_info.bus_type; }
-    hid_device_info *get_device_info_ptr() { return &hid_device_info; }
-    hid_device_info &get_device_info() { return hid_device_info; }
+    std::string get_path() {return device_info.path;}
+    unsigned short get_vendor_id() { return device_info.vendor_id; }
+    unsigned short get_product_id() { return device_info.product_id; }
+    std::wstring get_serial_number() { return device_info.serial_number; }
+    unsigned short get_release_number() { return device_info.release_number; }
+    std::wstring get_manufacturer_string() { return std::wstring(device_info.manufacturer_string); }
+    std::wstring get_product_string() { return std::wstring(device_info.product_string); }
+    unsigned short get_usage_page() { return device_info.usage_page; }
+    unsigned short get_usage() { return device_info.usage; }
+    int get_interface_number() { return device_info.interface_number; }
+    hid_bus_type get_bus_type() { return device_info.bus_type; }
+    hid_device_info *get_device_info_ptr() { return &device_info; }
+    hid_device_info &get_device_info() { return device_info; }
 
 private:
-    hid_device_info hid_device_info;
+    hid_device_info device_info;
 };
     
 #endif // _HIDAPI_PY_HID_DEVICE_INFO_H_
