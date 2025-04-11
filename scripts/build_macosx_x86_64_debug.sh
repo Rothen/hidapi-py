4738b3c1cef2e2d62efea6b3f00aa27919879dc1
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2025-04-11 09:08:55 +0200

    Add HIDAPI_ROOT definition to build script for macOS

--- scripts/build_macosx_x86_64_debug.sh
+++ scripts/build_macosx_x86_64_debug.sh
@@ -1,6 +1,7 @@
 cmake -S . -B build-debug -G Ninja \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_C_COMPILER=/usr/bin/clang \
-    -DCMAKE_CXX_COMPILER=/usr/bin/clang++
+    -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
+    -DHIDAPI_ROOT="$(brew --prefix hidapi)"
 
 cmake --build build-debug --config Debug --clean-first --target hidapi-py
